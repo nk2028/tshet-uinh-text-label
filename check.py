b90b31a126b03b00ae79c3392978d7904204410a
sgalal <sgalal.me@outlook.com>
2021-04-21 14:30:57 +0800

    Update text
    Reorder articles
    Change 生 to 三等

--- check.py
+++ check.py
@@ -1,4 +1,4 @@
-from QieyunEncoder import from描述
+import Qieyun
 import sys
 from xml.dom.minidom import parseString
 
@@ -13,7 +13,8 @@ with open('index.html') as f:
 		for rt in root.getElementsByTagName('rt'):
 			描述 = rt.firstChild.data
 			try:
-				from描述(描述) # function from描述 will perform checks on 描述
+				# this function will perform checks on 描述
+				Qieyun.音韻地位.from描述(描述)
 			except Exception as e:
 				print(描述, e, file=sys.stderr)
 				exit(1)
